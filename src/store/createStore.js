9a762a56adec123431707d1b58425c17ddc6bb27
eliton-algoll <eliton@algoll.com.br>
2020-03-17 19:35:29 -0300

    insert telas de administração

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,5 +1,6 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 
+// exportando a funcção createStore passando os reducers como parãmetro
 export default (reducers, middlewares) => {
   const enhancer =
     process.env.NODE_ENV === 'development'
